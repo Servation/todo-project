ea82fa6eb583d24f54570f7224c114e1a550d5ba
Servation <gwfds123@gmail.com>
2022-01-19 22:13:07 -0800

    added styles and complete button to todo

--- src/todo.js
+++ src/todo.js
@@ -1,11 +1,20 @@
 class Todo {
-    constructor(title, description, dueDate, priority, project, todoID) {
-        this.title = title
-        this.description = description
-        this.dueDate = dueDate
-        this.priority = priority
-        this.project = project
-        this.todoID = todoID
+    constructor(title, description, dueDate, priority, project, todoID, complete = false) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.project = project;
+        this.todoID = todoID;
+        this.complete = complete;
+    }
+
+    toggleComplete() {
+        if (this.complete) {
+            this.complete = false;
+        } else {
+            this.complete = true;
+        }
     }
 }
 
--- src/ui.js
+++ src/ui.js
@@ -1,10 +1,9 @@
 import Todo from "./todo";
 
-const todoArr = [];
+let todoArr = [];
 let TodoCounter = 0;
 
 export function addToDoFromEl() {
-    console.log("working");
     const titleEl = document.getElementById("title");
     const closeEl = document.getElementsByClassName("close");
     let close = closeEl[0];
@@ -17,7 +16,6 @@ export function addToDoFromEl() {
     if (valid) {
         addTodoToArray(title, description, dueDate, priority, project, TodoCounter);
         close.click();
-        console.log(todoArr);
         TodoCounter++;
         titleEl.style.backgroundColor = "white";
     } else {
@@ -39,8 +37,6 @@ function addTodoToArray(title, descrip, date, prio, proj, count) {
 
 function createSingleTodoContent(title, desc, date, prio, proj, count) {
     let todoContentEl = document.createElement("div");
-    todoContentEl.setAttribute("id", String(count));
-    todoContentEl.setAttribute("class", "singleTodo")
     let titleEl = document.createElement("h1");
     titleEl.textContent = title;
     titleEl.setAttribute("class", "todoTitle");
@@ -67,26 +63,53 @@ function createSingleTodoContent(title, desc, date, prio, proj, count) {
         projEl.setAttribute("class", "todoProjec");
         todoContentEl.appendChild(projEl);
     }
-
     return todoContentEl;
 }
 
 export function showAllTodo() {
-    const mainEl = document.getElementById("main")
+    const mainEl = document.getElementById("main");
     while (mainEl.firstChild) {
         mainEl.removeChild(mainEl.lastChild);
     }
     todoArr.forEach(element => {
+        let container = document.createElement("div");
+        container.setAttribute("id", String(element.todoID));
+        container.setAttribute("class", "singleTodo")
         let title = element.title;
         let description = element.description;
         let dueDate = element.dueDate;
         let prio = element.priority;
         let project = element.project;
-        mainEl.appendChild(createSingleTodoContent(title, description, dueDate, prio, project));
+        let id = element.todoID
+        let completeButton = document.createElement("button");
+        completeButton.textContent = "INCOMPELETE";
+        if (element.complete) {
+            container.style.backgroundColor = "gray"
+            completeButton.textContent = "COMPELETED";
+        } else {
+            container.style.backgroundColor = "cornflowerblue"
+            completeButton.textContent = "INCOMPELETE";
+        }
+        completeButton.addEventListener("click", () => {
+            element.toggleComplete();
+            showAllTodo();
+        })
+        let deleteButton = document.createElement("button");
+        deleteButton.textContent = "DELETE";
+        deleteButton.addEventListener("click", () => {
+            todoArr = todoArr.filter((obj) => {
+                return obj.todoID !== element.todoID;
+            });
+            showAllTodo();
+        })
+        container.appendChild(createSingleTodoContent(title, description, dueDate, prio, project, id))
+        container.appendChild(completeButton);
+        container.appendChild(deleteButton);
+        mainEl.appendChild(container);
     });
 
 }
 
 (() => {
-    addTodoToArray("Todo project", "", "", "Medium", "Odin Project", 1290129)
+    addTodoToArray("Todo project", "A project from Odin project", "2022-01-19", "Medium", "Odin Project", 1290129)
 })()
